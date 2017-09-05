39fd6b4c95925b69ca4b5bed3ae0db0ed76c318a
Robby Bourne <robbybourne1@gmail.com>
2017-09-05 10:37:19 -0400

    Setting up begining api

--- Controllers/DinosController.cs
+++ Controllers/DinosController.cs
@@ -2,37 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api_dotnet.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api_dotnet.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class DinoController : Controller
     {
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<DinosModel> Get()
         {
-            return new string[] { "value1", "value2" };
+            var dinos = new DinosModel();
+            List<DinosModel> DinoList = dinos.DinoListGetter();
+            return DinoList;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            var dinos = new DinosModel();
+            List<DinosModel> DinoList = dinos.DinoListGetter();
+            return DinoList[id-1];
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public void Post([FromBody]int id, string name, string color, string diet)
         {
+        var dinos = new DinosModel();
+        List<DinosModel> DinoList = dinos.DinoListGetter();
+        var newDino = new DinosModel
+           {
+            Id = id,
+            Name = name,
+            Color = color,
+            Diet = diet
+           };
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
+            var dinos = new DinosModel();
+            List<DinosModel> DinoList = dinos.DinoListGetter();   
         }
 
         // DELETE api/values/5
